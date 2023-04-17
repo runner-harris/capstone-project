a769d2eb422a49515d2d80946341406f55917018
purdywp <purdywp@g.cofc.edu>
2023-04-17 12:14:56 -0400

    dradis upload & download function, updated gitignore

--- coordinator/views.py
+++ coordinator/views.py
@@ -14,18 +14,22 @@ import json
 import os
 from django_q.tasks import async_task
 from tenable.io import TenableIO
+import paramiko # for ssh access
 
 # load apikeys from apikeys.json
 accesskey = os.getenv('TENABLE_ACCESS_KEY')
 secretkey = os.getenv('TENABLE_SECRET_KEY')
-
 api_token = os.getenv('DRADIS_API_KEY')
+
 url = 'https://cofc-dradis.soteria.io'
 dradis_api = Dradis(api_token, url)
 projects = dradis_api.get_all_projects()
 
 # instantiate tenable object
 tio = TenableIO(accesskey, secretkey)
+
+# instantiate paramiko object
+ssh = paramiko.SSHClient()
 
 class ScanList(generics.CreateAPIView):
     queryset = Scan.objects.all()
@@ -57,19 +61,8 @@ class ScanList(generics.CreateAPIView):
         )
         tio.scans.launch(scan['id'])
 
-        #download_scan(scan['id'],accesskey,secretkey)
-        async_task(download_scan,scan['id'],accesskey,secretkey)
+        async_task(download_scan,scan['id'], accesskey, secretkey, api_token, scan_name)
 
-
-        # assuming status is 'completed':
-        # download nessus file
-        # with open(str(scan['id']) + '.nessus', 'wb') as reportobj:
-        #     print(id)
-        #     results = tio.scans.export(scan['id'],fobj=reportobj)
-
-
-        # TODO 
-        dradis_api.create_project(scan_name, 42, 0, [], 'Vulnerability Scan Project Template v1')
         return Response({'message': 'Scan created and run successfully'})
     
 
